13fc8224ad28f3b5fb3d14c78b09b41c4bda60fd
Victor Anogwih <anogwihvictor@gmail.com>
2024-02-01 12:19:23 -0500

    tweeter updates

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+  
+  const characterLimit = 140;
+
+  //Event listener for textarea
+  $('#tweet-text').on('input', function() {
+    console.log(this);
+    const characters = characterLimit - $(this).val().length;
+    
+    //Display the remaining Characters with class = counter
+    $('.counter').text(characters);
+
+    //Adjust Counter Color based on character limit
+    if(characters < 0) {
+      $('.counter').addClass('redCounter');
+    } else {
+      $('.counter').removeClass('redCounter');
+    }
+
+  })
+})
+  
